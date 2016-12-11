e192b9dfab92be27e2dad2ec31d6993dd78acf5a
Paravion <henrylin@Henrys-MacBook-Pro.local>
2016-12-12 01:37:10 +0800

    Almost redo the whole report

--- pcexp7csv.R
+++ pcexp7csv.R
@@ -2,6 +2,8 @@
 
 setwd("PChemLab2/data/exp7")
 
+a <-1
+
 for (a in 1:5){
   filename <- paste("a",a,"-1",".txt",sep="") 
   data1 <- read.table(filename)
@@ -12,31 +14,32 @@ for (a in 1:5){
   time <- (data1[aa,1]+data2[bb,1])/2
   intensity <- (data1[aa,2]+data2[bb,2])/2
   data <- data.frame(time,intensity)
-  t <- length(data$time)
-  it <- data$intensity[t] #it = intensity_t
-  A <- log10(data$intensity/it) #A = intensity / intensity_t
+  it <- data$intensity[1] #it = intensity_t
+  A <- log10(it/data$intensity) #A = intensity_t / intensity
   full <- data.frame(time,intensity,A) #full = full data
   newfile <- paste("A",a,".csv",sep="")
   write.csv(full,file=newfile,row.names=FALSE)
   a <- a+1
+  if (a >5){break}
  }
- 
+
+#calulate ln(Aeq-At)-t
+
+index <- 1:5
+aeq <- c(1.06,1.50,1.41,1.43,1.14)
+aeq <- data.frame(index,aeq)
+
+a <- 1
 for (a in 1:5){
-  filename <- paste("a",a,"-1",".txt",sep="") 
-  data1 <- read.table(filename)
-  filename <- paste("a",a,"-2",".txt",sep="")
-  data2 <- read.table(filename)
-  aa <- data1[,1] > 0
-  bb <- data2[,1] > 0
-  time <- (data1[aa,1]+data2[bb,1])/2
-  intensity <- (data1[aa,2]+data2[bb,2])/2
-  data <- data.frame(time,intensity)
-  t <- length(data$time)
-  it <- data$intensity[t] #it = intensity_t
-  A <- log10(data$intensity/it) #A = intensity / intensity_t
-  aa <- A != 0
-  full <- data.frame(time[aa],intensity[aa],A[aa]) #full = full data
-  newfile <- paste("A",a,"aa",".csv",sep="")
-  write.csv(full,file=newfile,row.names=FALSE)
+  filename <- paste("a",a,"aa",".csv",sep="") 
+  data <- read.csv(filename)
+  time <- data$time
+  lnaeq <- log(aeq$aeq[a] - data$intensity) #lnaeq = ln(Aeq-At)-t
+  data <- data.frame(time,lnaeq)
+  newfile <- paste("A",a,"ln",".csv",sep="")
+  write.csv(data,file=newfile,row.names=FALSE)
   a <- a+1
+  if (a >5){break}
 }
+
+
